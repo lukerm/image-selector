f0d07b404fa429da48cc47e7ecc36f5094de3250
lukerm <13883308+lukerm@users.noreply.github.com>
2019-05-31 06:21:30 +0100

    create a static 3x3 grid of the same image

--- selector_app.py
+++ selector_app.py
@@ -8,6 +8,34 @@ import flask
 
 app = dash.Dash(__name__)
 
+
+def create_image_grid(img):
+    """
+    Create a 3x3 grid of the same image img
+    """
+    grid = html.Div(
+        html.Table([
+            html.Tr([
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+            ]),
+            html.Tr([
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+            ]),
+            html.Tr([
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+                html.Td(html.Div(html.Img(src=img), style={'padding': 0})),
+            ]),
+        ])
+    )
+
+    return grid
+
+
 # Assumes that images are stored in the img/ directory for now
 image_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
 static_image_route = '/static/'
@@ -16,7 +44,7 @@ static_image_route = '/static/'
 app.layout = html.Div(
     children=[
         html.H2("Happy Frog"),
-        html.Img(src=static_image_route + 'happyFrog.jpg')
+        html.Div(create_image_grid(static_image_route + 'happyFrog.jpg'))
     ]
 )
 
