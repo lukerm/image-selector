ee4e1106b8bf2f8cc5cef606ea73bab52e43f9fe
lukerm <13883308+lukerm@users.noreply.github.com>
2019-06-07 23:18:40 +0100

    choose the size of the left-hand grid based on a dropdown menu

--- selector_app.py
+++ selector_app.py
@@ -3,24 +3,11 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 import flask
 
 app = dash.Dash(__name__)
-
-
-def create_image_grid(img, n_row, n_col):
-    """
-    Create a grid of the same image img with n_row rows and n_col columns
-    """
-    pad = 2
-    img_style = {'display': 'block', 'height': 'auto', 'max-width': '100%'}
-
-    grid_element = html.Td(html.Div(html.Img(src=img, style=img_style), style={'padding': pad}))
-    one_row = html.Tr([grid_element] * n_col)
-    grid = html.Div(html.Table([one_row] * n_row))
-
-    return grid
 
 
 # Assumes that images are stored in the img/ directory for now
@@ -32,22 +19,61 @@ n_row, n_col = 7, 5
 app.layout = html.Div(
     children=[
         html.H2("Happy Frogs"),
+        dcc.Dropdown(
+            id='choose-image',
+            options=[{'label': 'happy frog original', 'value': 'happyFrog.jpg'},],
+            value='happyFrog.jpg',
+            style={'width': '10vw',}
+        ),
+        dcc.Dropdown(
+            id='choose-grid-size',
+            options=[
+                {'label': '2 x 2', 'value': 2},
+                {'label': '3 x 3', 'value': 3},
+                {'label': '4 x 4', 'value': 4},
+                {'label': '5 x 5', 'value': 5},
+            ],
+            value=2,
+            style={'width': '10vw'}
+        ),
         html.Div([
             html.Table([
                 html.Tr([
                     html.Td(
-                        create_image_grid(static_image_route + 'happyFrog.jpg', n_row, n_col),
+                        id='responsive-frogs',
                         style={'width': '50vw', 'height': 'auto', 'border-style': 'solid',}
                         ),
-                    html.Td(
-                        create_image_grid(static_image_route + 'happyFrog.jpg', n_row, n_col),
-                        style={'width': '50vw', 'height': 'auto', 'border-style': 'solid',}
-                        ),
+#                    html.Td(
+#                        create_image_grid(static_image_route + 'happyFrog.jpg', n_row, n_col),
+#                        style={'width': '50vw', 'height': 'auto', 'border-style': 'solid',}
+#                        ),
                 ]),
             ]),
         ]),
     ]
 )
+
+
+@app.callback(
+    Output('responsive-frogs', 'children'),
+    [Input('choose-image', 'value'),
+     Input('choose-grid-size', 'value'),
+     Input('choose-grid-size', 'value')]
+)
+def create_image_grid(img_fname, n_row, n_col):
+    """
+    Create a grid of the same image img with n_row rows and n_col columns
+    """
+    img_path = static_image_route + img_fname
+    pad = 2
+    img_style = {'display': 'block', 'height': 'auto', 'max-width': '100%'}
+
+    grid_element = html.Td(html.Div(html.Img(src=img_path, style=img_style), style={'padding': pad}))
+    one_row = html.Tr([grid_element] * n_col)
+    grid = html.Div(html.Table([one_row] * n_row))
+
+    return grid
+
 
 @app.server.route('{}<image_path>'.format(static_image_route))
 def serve_image(image_path):
