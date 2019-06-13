3802d69a6f91f57bd33327d87752257b5139d14f
lukerm <13883308+lukerm@users.noreply.github.com>
2019-06-13 22:27:25 +0100

    define all grid elements up front in order to assign callbacks to them (currently a bit slow)

--- selector_app.py
+++ selector_app.py
@@ -14,6 +14,9 @@ app = dash.Dash(__name__)
 image_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'img')
 static_image_route = '/static/'
 
+# Define the maximal grid dimensions
+ROWS_MAX, COLS_MAX = 7, 7
+
 
 def create_image_grid(img_fname, n_row, n_col):
     """
@@ -23,22 +26,30 @@ def create_image_grid(img_fname, n_row, n_col):
     pad = 2
     img_style = {'display': 'block', 'height': 'auto', 'max-width': '100%'}
 
-    def get_grid_element(x, y):
+    def get_grid_element(x, y, hidden):
+
+        # Set the display to none if this grid cell is hidden
+        if hidden:
+            style = {'padding': 0, 'display': 'none'}
+        else:
+            style = {'padding': pad}
+
         my_id = f'{x}-{y}'
         return html.Td(id='grid-td-' + my_id,
                        className='focus-off',
                        children=html.Button(id='grid-button-' + my_id,
                                             children=html.Img(src=img_path, style=img_style),
-                                            style={'padding': pad},
+                                            style=style,
                                             ),
                        style={'border-color': 'white'} # focus off at beginning
                        )
 
     grid = []
-    for i in range(n_row):
+    for i in range(ROWS_MAX):
         row = []
-        for j in range(n_col):
-            row.append(get_grid_element(i, j))
+        for j in range(COLS_MAX):
+            hidden = (i >= n_row) or (j >= n_col)
+            row.append(get_grid_element(i, j, hidden))
         row = html.Tr(row)
         grid.append(row)
 
@@ -59,12 +70,7 @@ app.layout = html.Div(
             ),
             dcc.Dropdown(
                 id='choose-grid-size',
-                options=[
-                    {'label': '2 x 2', 'value': 2},
-                    {'label': '3 x 3', 'value': 3},
-                    {'label': '4 x 4', 'value': 4},
-                    {'label': '5 x 5', 'value': 5},
-                ],
+                options=[{'label': f'{k+1} x {k+1}', 'value': k+1} for k in range(ROWS_MAX) if k > 0],
                 value=2,
                 style={'width': '5vw', 'display': 'inline-block'}
             ),
@@ -88,7 +94,6 @@ app.layout = html.Div(
 )
 
 
-
 @app.callback(
     Output('responsive-frogs', 'children'),
     [Input('choose-image', 'value'),
@@ -99,66 +104,35 @@ def create_reactive_image_grid(img_fname, n_row, n_col):
     return create_image_grid(img_fname, n_row, n_col)
 
 
-#@app.callback(
-#    Output('grid-td-0-0', 'className'),
-#    [Input('grid-button-0-0', 'n_clicks')]
-#)
-#def change_style(n):
-#    if n is None or n % 2 == 0:
-#        return 'focus-off'
-#    else:
-#        return 'focus-on'
+# Create callbacks for all grid elements (hidden and visible)
+# As they are all defined in advance, all grid ids exist from the beginning (i.e. in the static app.layout)
+grid_table = app.layout.get('responsive-frogs').children.children
+for i in range(ROWS_MAX):
+    for j in range(COLS_MAX):
 
+        # We can only set a callback on an element once, so we first check to if it has already been assigned
+        if f'grid-td-{i}-{j}.style' not in app.callback_map:
+            assert f'grid-td-{i}-{j}.className' not in app.callback_map
 
-#@app.callback(
-#    Output('grid-td-0-0', 'style'),
-#    [Input('grid-button-0-0', 'n_clicks')]
-#)
-#def change_style(n):
-#    # TODO: control via className and CSS
-#    if n is None or n % 2 == 0:
-#        return {'border-color': 'white', 'border-style': 'solid'}
-#    else:
-#        return {'border-color': 'red', 'border-style': 'solid'}
+            @app.callback(
+                Output(f'grid-td-{i}-{j}', 'style'),
+                [Input(f'grid-button-{i}-{j}', 'n_clicks')]
+            )
+            def change_style(n):
+                if n is None or n % 2 == 0:
+                    return {'border-color': 'white', 'border-style': 'solid'}
+                else:
+                    return {'border-color': 'red', 'border-style': 'solid'}
 
-
-# attach responsive elements to grid
-# TODO: only works for 2x2 case
-@app.callback(
-    Output('hidden-div', 'children'),
-    [Input('responsive-frogs', 'children')]
-)
-def update_grid_callbacks(grid):
-
-    grid_table = grid['props']['children'] # Retrieves table element's children
-    for i, row in enumerate(grid_table['props']['children']):
-        for j, col in enumerate(row['props']['children']):
-
-            # We can only set a callback on an element once, so we first check to if it has already been assigned
-            if f'grid-td-{i}-{j}.style' not in app.callback_map:
-                assert f'grid-td-{i}-{j}.className' not in app.callback_map
-
-                @app.callback(
-                    Output(f'grid-td-{i}-{j}', 'style'),
-                    [Input(f'grid-button-{i}-{j}', 'n_clicks')]
-                )
-                def change_style(n):
-                    if n is None or n % 2 == 0:
-                        return {'border-color': 'white', 'border-style': 'solid'}
-                    else:
-                        return {'border-color': 'red', 'border-style': 'solid'}
-
-                @app.callback(
-                    Output(f'grid-td-{i}-{j}', 'className'),
-                    [Input(f'grid-button-{i}-{j}', 'n_clicks')]
-                )
-                def change_class_name(n):
-                    if n is None or n % 2 == 0:
-                        return 'focus-off'
-                    else:
-                        return 'focus-on'
-
-    return None
+            @app.callback(
+                Output(f'grid-td-{i}-{j}', 'className'),
+                [Input(f'grid-button-{i}-{j}', 'n_clicks')]
+            )
+            def change_class_name(n):
+                if n is None or n % 2 == 0:
+                    return 'focus-off'
+                else:
+                    return 'focus-on'
 
 
 @app.server.route('{}<image_path>'.format(static_image_route))
@@ -171,6 +145,7 @@ def serve_image(image_path):
     #if image_name not in list_of_images:
     #    raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
     return flask.send_from_directory(image_directory, image_name)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
